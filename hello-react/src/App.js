295e678204a213fe1a307eed15c6f7493efb4799
hotsixredbull <akkalee1207@gmail.com>
2020-03-08 17:39:33 +0900

    end of 4th chapter: Added Counter

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyName from './NewComponent'
+import Date from './FunctionTypeComponent'
+import Counter from './Counter'
 
 function App() {
+  const value = 1;
+  console.log(value);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +23,25 @@ function App() {
         >
           Learn React
         </a>
+        {
+          1 + 1 === 2 
+            ? (<div>맞아요!</div>)
+            : (<div>틀려요!</div>)
+        }
+        {
+          1 + 1 === 2 && (<div>맞아요!</div>)
+        }
+        {
+          (function() {
+            if (value === 1) return (<div>하나</div>);
+            if (value === 2) return (<div>둘</div>);
+            if (value === 3) return (<div>셋</div>);
+          })()
+        }
+        {/* 주석은 이렇게! */}
+        <MyName name="Alan" />
+        <Date date="2020-03-08"/>
+        <Counter />
       </header>
     </div>
   );
---
+++ hello-react/src/FunctionTypeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Date = ({ date }) => {
+  return (
+    <div>
+      이 프로젝트의 업데이트 날짜는 {date} 입니다.
+    </div>
+  );
+};
+
+export default Date;
