aed524c8c839a1d7b4d4d0258d208812a9033039
Alan Seunggi Lee <software.lee@samsung.com>
2021-05-11 23:02:03 +0900

    update until the 2nd lecture

---
+++ inflearn/babel/simple1.js
@@ -0,0 +1,20 @@
+function LikeButton() {
+    const [liked, setLiked] = React.useState(false);
+    const text = liked ? '좋아요 취소' : '좋아요';
+    return <button onClick={() => setLiked(!liked)}>{text}</button>
+}
+
+function Container() {
+    const [count, setCount] = React.useState(0);
+    return (
+        <div>
+            <LikeButton />
+            <div>
+                <span>현재 카운트: </span>
+                <span style={{marginRight: 10}}>{count}</span>
+                <button onClick={() => setCount(count+1)}>증가</button>
+                <button onClick={() => setCount(count-1)}>감소</button>
+            </div>
+        </div>
+    );
+}
