228635caf2c69eeded532252a103606f09fb5368
Alan Seunggi Lee <software.lee@samsung.com>
2021-05-19 14:51:36 +0900

    custom hook

---
+++ inflearn/hook/customHook/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useUser from './useUser';
+
+export default function Profile({ userId }) {
+    const user = useUser(userId);
+    return (
+        <div>
+            {!user && <p>사용자 정보를 가져오는 중...</p>}
+            {user && (
+                <>
+                    <p>{`name is ${user.name}`}</p>
+                    <p>{`age is ${user.age}`}</p>
+                </>
+            )}
+        </div>
+    )
+}
